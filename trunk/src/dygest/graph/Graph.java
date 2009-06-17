3c825e333127158d6470f64591efe4a5c0cf6697
anand@semanticvoid.com <anand@semanticvoid.com@7b53f254-557d-11de-8b28-e1b452efc53a>
2009-06-17 07:41:33 +0000

    Update issue 6: Changes: - Implemented Comparable interface for Graph - Completed the GraphScorer Class (now need to look at specific scoring function implementations)

--- trunk/src/dygest/graph/Graph.java
+++ trunk/src/dygest/graph/Graph.java
@@ -8,6 +8,28 @@ package dygest.graph;
  * @author anand
  *
  */
-public class Graph {
+public class Graph implements Comparable<Graph> {
 	
+	private double score = 0;
+	
+	public void setScore(double score) {
+		this.score = score;
+	}
+	
+	public double getScore() {
+		return this.score;
+	}
+
+	/**
+	 * The comparison function
+	 */
+	public int compareTo(Graph o) {
+		if(this.score < o.getScore()) {
+			return -1;
+		} else if(this.score > o.getScore()) {
+			return 1;
+		} else {
+			return 0;
+		}
+	}
 }

7afac00f40b546ccc2fc637589d773097fc563fb
anand@semanticvoid.com <anand@semanticvoid.com@7b53f254-557d-11de-8b28-e1b452efc53a>
2009-06-17 07:41:33 +0000

    Update issue 6: Changes: - Implemented Comparable interface for Graph - Completed the GraphScorer Class (now need to look at specific scoring function implementations)

--- src/dygest/graph/GraphScorer.java
+++ src/dygest/graph/GraphScorer.java
@@ -3,10 +3,53 @@
  */
 package dygest.graph;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import dygest.graph.score.IScore;
+
 /**
+ * This class implements the scorer for scoring a set of graphs.
  * @author anand
  *
  */
-public class GraphScorer {
+public class GraphScorer implements IGraphScorer {
+	
+	private IScore scoringFunction;
+	private List<Graph> graphs;
+	
+	/**
+	 * The constructor
+	 * @param scoringFunction	the scoring mechanism implementation
+	 */
+	public GraphScorer(IScore scoringFunction, List<Graph> graphs) {
+		this.scoringFunction = scoringFunction;
+		this.graphs = graphs;
+	}
+	
+	/**
+	 * This function iterates through all the Graphs and scores them
+	 * as per the supplied scoring mechanism.
+	 */
+	public void scoreGraphs() {
+		for(Graph g : this.graphs) {
+			double score = this.scoringFunction.score(g);
+			g.setScore(score);
+		}
+		
+		java.util.Collections.sort(this.graphs);
+	}
+	
+	/**
+	 * @see dygest.graph.IGraphScorer#getGraphAtRank
+	 */
+	public Graph getGraphAtRank(int rank) {
+		if(rank <= 0 || rank > this.graphs.size()) {
+			return null;
+		}
+		
+		return this.graphs.get(rank-1);
+	}
 
 }

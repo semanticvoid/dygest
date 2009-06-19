56b91eb3e39f45885d9b858d638f1cc1b78d4ec4
anand@semanticvoid.com <anand@semanticvoid.com@7b53f254-557d-11de-8b28-e1b452efc53a>
2009-06-19 04:08:01 +0000

    Update issue 5: Completed Graph Class + Node Class with linkages

--- trunk/src/dygest/graph/Graph.java
+++ trunk/src/dygest/graph/Graph.java
@@ -3,6 +3,12 @@
  */
 package dygest.graph;
 
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+import dygest.text.Word;
+
 /**
  * This class represents the Graph data structure.
  * @author anand
@@ -10,7 +16,65 @@ package dygest.graph;
  */
 public class Graph implements Comparable<Graph> {
 	
+	private long id;
+	private int nodeCount = 0;
+	private Set<Node> nodes;
+	private Set<Edge> edges;
 	private double score = 0;
+	
+	public Graph() {
+		// generate a unique id for this graph
+		// currently the number of milliseconds since epoch
+		this.id = new Date().getTime();
+		this.nodes = new HashSet<Node>();
+		this.edges = new HashSet<Edge>();
+	}
+	
+	/**
+	 * Create a node in the graph representing the Word
+	 * @param w	the Word obj
+	 * @return	the Node obj on success, null otherwise
+	 */
+	public Node createNode(Word w) {
+		Node node = new Node(++this.nodeCount, w);
+		if(addNode(node)) {
+			return node;
+		}
+		
+		return null;
+	}
+	
+	/**
+	 * Create an edge between two Nodes
+	 * @param node1	the first Node
+	 * @param node2	the second Node
+	 * @param weight	the weight of the edge
+	 * @return	true on success, false otherwise
+	 */
+	public boolean createLink(Node node1, Node node2, double weight) {
+		Edge e = new Edge(node1.getID(), node2.getID(), weight);
+		node1.addEdge(e);
+		node2.addEdge(e);
+		return addLink(e);
+	}
+	
+	/**
+	 * Add node to set of nodes
+	 * @param node	the Node obj
+	 * @return	true on success, false otherwise
+	 */
+	private boolean addNode(Node node) {
+		return this.nodes.add(node);
+	}
+	
+	/**
+	 * Add edge to set of edges
+	 * @param e	the Edge obj
+	 * @return	true on success, false otherwise
+	 */
+	private boolean addLink(Edge e) {
+		return this.edges.add(e);
+	}
 	
 	public void setScore(double score) {
 		this.score = score;

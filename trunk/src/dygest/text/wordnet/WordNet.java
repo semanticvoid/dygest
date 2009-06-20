0ad836d02d603b3ec4894adabb54454ce1eea73b
anand@semanticvoid.com <anand@semanticvoid.com@7b53f254-557d-11de-8b28-e1b452efc53a>
2009-06-20 06:31:15 +0000

    Update issue 5: Singleton wrapper for WordNet library.

---
+++ trunk/src/dygest/text/wordnet/WordNet.java
@@ -0,0 +1,47 @@
+/**
+ * 
+ */
+package dygest.text.wordnet;
+
+import java.net.URL;
+
+import edu.mit.jwi.Dictionary;
+import edu.mit.jwi.IDictionary;
+
+
+/**
+ * This class is the local wrapper over the WordNet library (so that
+ * other classes are free from specific implementation details of the
+ * library).
+ * This is a singleton class.
+ * @author anand
+ *
+ */
+public class WordNet {
+
+	private WordNet wn = null;
+	private IDictionary dict = null;
+	
+	/**
+	 * Private constructor (for singleton)
+	 */
+	private WordNet() throws Exception {
+		// @TODO move this path to the Environment variable WNHOME
+		URL url = new URL("file", null, "/usr/local/WordNet-3.0/dict/");
+		this.dict = new Dictionary(url);
+		this.dict.open();
+	}
+	
+	/**
+	 * Method that returns the singleton WordNet obj
+	 * @return the singleton object
+	 */
+	public WordNet getSingleton() throws Exception {
+		if(this.wn == null) {
+			this.wn = new WordNet();
+		}
+		
+		return this.wn;
+	}
+	
+}

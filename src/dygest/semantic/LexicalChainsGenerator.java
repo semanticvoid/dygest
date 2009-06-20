ed4ee1b62c455f7a834d4f5d9ac6576cf6a15f79
anand@semanticvoid.com <anand@semanticvoid.com@7b53f254-557d-11de-8b28-e1b452efc53a>
2009-06-20 10:12:42 +0000

    Update issue 5: added LexicalChainGenerator class + interface. Added hypernym function to WordNet wrapper - need to implement it.

---
+++ src/dygest/semantic/LexicalChainsGenerator.java
@@ -0,0 +1,59 @@
+/**
+ * 
+ */
+package dygest.semantic;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import jsl.measure.jwi.JWIAbstractSemanticMeasure;
+import dygest.graph.Graph;
+import dygest.text.Word;
+
+/**
+ * This class operates on the input words and generates a semantic 
+ * concept graphs for those words. 
+ * @author anand
+ *
+ */
+public class LexicalChainsGenerator implements IInterpretation {
+	
+	private Set<Graph> interpretations;
+	private List<Word> words;
+	private JWIAbstractSemanticMeasure semanticMeasure;
+	
+	/**
+	 * Constructor - works on the candidate words
+	 * @param words	the candidate words
+	 * @param semanticMeasure	the metric to be used as a semantic measure
+	 */
+	public LexicalChainsGenerator(List<Word> words, JWIAbstractSemanticMeasure semanticMeasure) {
+		this.semanticMeasure = semanticMeasure;
+		this.words = words;
+		this.interpretations = new HashSet<Graph>();
+	}
+	
+	/**
+	 * This function call populates various interpretations for the
+	 * given words based on WordNet and the semantic measure. This needs
+	 * to be called immediately after the constructor.
+	 */
+	public void interpret() {
+		
+	}
+
+	/**
+	 * @see dygest.semantic.IInterpretation#getInterpretationAtRank(int)
+	 */
+	public Graph getInterpretationAtRank(int rank) {
+		return null;
+	}
+
+	/**
+	 * @see dygest.semantic.IInterpretation#getAllInterpretations()
+	 */
+	public Set<Graph> getAllInterpretations() {
+		return this.interpretations;
+	}
+}

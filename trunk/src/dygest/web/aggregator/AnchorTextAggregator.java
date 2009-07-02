577aa91b3904d5917f9fb04c599f07bd68827b39
anand@semanticvoid.com <anand@semanticvoid.com@7b53f254-557d-11de-8b28-e1b452efc53a>
2009-07-02 02:56:24 +0000

    Update issue 9: - added HTTP helper functions - added class for aggregating anchor texts for given url

---
+++ trunk/src/dygest/web/aggregator/AnchorTextAggregator.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package dygest.web.aggregator;
+
+/**
+ * This class implements the collection of anchor texts for a given URL.
+ * It uses the Yahoo! Site Explorer API to search for inlinks.
+ * @author anand
+ *
+ */
+public class AnchorTextAggregator {
+
+	public AnchorTextAggregator() {
+		
+	}
+	
+	
+}

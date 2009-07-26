5335cbced57d14bd7045c55527a723b27ea43402
sudheer.624 <sudheer.624@7b53f254-557d-11de-8b28-e1b452efc53a>
2009-07-26 03:44:44 +0000

    comments some unused variables

--- trunk/src/dygest/datatype/Word.java
+++ trunk/src/dygest/datatype/Word.java
@@ -3,12 +3,14 @@ package dygest.datatype;
 import edu.mit.jwi.item.ISynsetID;
 
 public class Word {
+	//The text of the chunk.
 	private String wordName;
+	
 	private int wordFrequency = 0;
 	private double probability = 0;
-	private int semanticFrequency = 0;
+	//private int semanticFrequency = 0;
 	private boolean candidateWord = false;
-	private int coverageFrequency = 0;
+	//private int coverageFrequency = 0;
 	private String wordPOS = null;
 	private String stemmedVersion = null;
 	private ISynsetID sense = null;
@@ -24,19 +26,19 @@ public class Word {
 	public int getWordFrequency() {
 		return wordFrequency;
 	}
-	
+/*	
 	public int getSemanticFrequency() {
 		return semanticFrequency;
 	}
-
+*/
 	public boolean isCandidateWord() {
 		return candidateWord;
 	}
-
+/*
 	public int getCoverageFrequency() {
 		return coverageFrequency;
 	}
-
+*/
 	public String getWordPos() {
 		return wordPOS;
 	}
@@ -60,19 +62,19 @@ public class Word {
 	public void setWordFrequency(int wordFrequency) {
 		this.wordFrequency = wordFrequency;
 	}
-	
+/*	
 	public void setSemanticFrequency(int semanticFrequency) {
 		this.semanticFrequency = semanticFrequency;
 	}
-
+*/
 	public void setCandidateWord(boolean candidateWord) {
 		this.candidateWord = candidateWord;
 	}
-
+/*
 	public void setCoverageFrequency(int coverageFrequency) {
 		this.coverageFrequency = coverageFrequency;
 	}
-
+*/
 	public void setWordPOS(String wordPOS) {
 		this.wordPOS = wordPOS;
 	}
